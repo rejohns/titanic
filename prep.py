a3d2b8ec7d66848b44a6b89d1403ebe02a4896b7
erwityk <erwityk@email.wm.edu>
2017-06-19 16:00:58 -0400

    Feature normalization function

--- prep.py
+++ prep.py
@@ -27,13 +27,19 @@ def _dummy(column):
     """Returns columns of dummy variables from a column of categories"""
     pass
 
-def _normalize(min_val=None, max_val=None):
+def _normalize(column, min_val=None, max_val=None):
     """Normalizes a column of data
 
     If no min and max values are given, then the data is normalized based off
     of the min and max from the sample data.
     """
-    pass
+    if min_val is None:
+        min_val = min(column)
+    if max_val is None:
+        max_val = max(column)
+    column = [float(val) for val in column]
+    column = [(val - min_val) / (max_val - min_val) for val in column]
+    return column
 
 def parse_data(path):
     """Cleans and prepares the data to be used in the  model"""
